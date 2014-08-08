75e8f2af16dccc42fdde9d5d5297ba2ce690b82a
Ben Lynn <benlynn@gmail.com>
2014-08-07 22:28:44 -0700

    Minor refactor.

--- t.hs
+++ t.hs
@@ -6,20 +6,20 @@ import Data.Tree
 import Control.Monad
 import Haste
 import Haste.Graphics.Canvas
-import System.IO.Unsafe
 
 sz = 64
 bd = 4
 bnds = ((0,0), (2,2))
+moveRandomly = False
 
 data Event = KeyDown Int | MouseDown Int Int
 
-data Status = Draw | Won | Play deriving (Eq, Show)
+data Status = Draw | Won | Play deriving Eq
 
 data Game = Game { board :: Array (Int, Int) Char
                  , status :: Status
                  , player :: Char
-                 } deriving Show
+                 }
 
 initGame = Game (array bnds [(i, '.') | i <- range bnds]) Play 'X'
 
@@ -30,7 +30,7 @@ oblong x y w h = fill $ rect (fromIntegral x, fromIntegral y) (fromIntegral (x +
 nextPlayer 'X' = 'O'
 nextPlayer 'O' = 'X'
 
-statusCheck game@(Game board Play player) =
+move (Game board0 Play player) i = let board = board0 // [(i, player)] in
   if or $      and [board!(x,x) == player | x <- [0..2]]
         :      and [board!(x,2 - x) == player | x <- [0..2]]
         :  map and [[board!(x,y) == player | x <- [0..2]] | y <- [0..2]]
@@ -42,35 +42,29 @@ statusCheck game@(Game board Play player) =
     Game board Play (nextPlayer player)
 
 nextMoves game@(Game board status player) = (game, case status of
-  Draw -> []
-  Won -> []
-  Play -> [statusCheck $ Game (board // [(i, player)]) Play player | i@(x, y) <- range bnds, board!i == '.'])
+  Play -> [move game i | i <- range bnds, board!i == '.']
+  _ -> [])
 
 gameTree = unfoldTree nextMoves 
 
-maximize :: Tree Game -> Int
-maximize (Node game@(Game _ status player) []) = case status of
-  Draw -> 0
+score game@(Game _ status player) = case status of
   Won -> if player == 'X' then -1 else 1
-maximize (Node _ sub) = maximum (map minimize sub)
+  _ -> 0
 
-minimize :: Tree Game -> Int
-minimize (Node game@(Game _ status player) []) = case status of
-  Draw -> 0
-  Won -> if player == 'X' then -1 else 1
-minimize (Node _ sub) = minimum (map maximize sub)
+maximize (Node leaf []) = score leaf
+maximize (Node _ kids) = maximum (map minimize kids)
 
-best :: [Game] -> Int -> Game -> Game
-best [] n bestYet = bestYet
-best (x:xs) n bestYet = let n' = minimize ( gameTree x ) in if n' > n then best xs n' x else best xs n bestYet
+minimize (Node leaf []) = score leaf
+minimize (Node _ kids) = minimum (map maximize kids)
 
-main = do
+best (x:xs) = let
+  f [] n bestYet = bestYet
+  f (x:xs) n bestYet = let n' = minimize ( gameTree x ) in if n' > n then f xs n' x else f xs n bestYet
+  in f xs (minimize $ gameTree x) x
+
+main = withElems ["body", "canvas", "message"] $ \[body, canvasElem, message] -> do
   xo <- loadBitmap "xo.png"
-  Just canvasElem <- elemById "canvas"
   Just canvas <- getCanvas canvasElem
-  Just body <- elemById "body"
-  Just stateElem <- elemById "state"
-  Just debugElem <- elemById "debug"
   evq <- newEmptyMVar
   putMVar evq []
   canvasElem  `onEvent` OnMouseDown $ \_button (x, y) -> do
@@ -89,6 +83,14 @@ main = do
         putMVar seedVar seed1
         return a
 
+    shuffleIO :: [a] -> IO [a]
+    shuffleIO [] = return []
+    shuffleIO xs = do
+      n <- randomRIO (0, length xs - 1)
+      let (a, b:bs) = splitAt n xs in do 
+        ys <- shuffleIO (a ++ bs)
+        return (b:ys)
+
     sq :: Char -> Int -> Int -> Picture ()
     sq c x y = do
       -- Draw borders.
@@ -102,33 +104,30 @@ main = do
 
     handle game@(Game board status player) (MouseDown x y) = let
       j = (x `div` sz, y `div` sz)
-      in if status == Play && inRange bnds j && board!j == '.' then statusCheck $ Game (board // [(j, player)]) Play player else game
+      in if status == Play && inRange bnds j && board!j == '.' then move game j else game
 
     handle game (KeyDown sym) = case sym of
-      68 -> unsafePerformIO $ do
-        setProp debugElem "innerHTML" $ show $ map (map (\x -> (x, minimize (gameTree x))))$ take 3 $ levels $ gameTree game
-        return game
       113 -> initGame
       _ -> game
 
     aiMove :: Game -> IO Game
-    aiMove game@(Game _ Play 'O') = let moves = snd $ nextMoves game in return $ best (tail moves) (minimize $ gameTree $ head moves) (head moves)
-    {-
-    aiMove game@(Game board Play 'O') = let moves = [(x, y) | (x, y) <- range bnds, board!(x, y) == '.'] in do
-      n <- randomRIO (0, length moves - 1)
-      return $ statusCheck $ Game (board // [(moves!!n, 'O')]) Play 'O'
-      -}
+    aiMove game@(Game _ Play 'O') = let moves = snd $ nextMoves game in
+      if moveRandomly then do
+        n <- randomRIO (0, length moves - 1)
+        return $ moves!!n
+      else do
+        shuffledMoves <- shuffleIO moves
+        return $ best shuffledMoves
     aiMove game = return game
 
     loop game = do
-      q <- takeMVar evq
-      putMVar evq []
+      q <- swapMVar evq []
       let game1@(Game board status player) = if null q then game else handle game (head q) in do
         render canvas $ sequence_ [sq (board!i) x y | i@(x, y) <- range bnds] 
-        setProp stateElem "innerHTML" $ case status of
+        setProp message "innerHTML" $ case status of
           Won -> player : " wins"
           Draw -> "Draw"
-          Play -> player : " to move"
+          Play -> if player == 'X' then "X to move" else "Thinking..."
         game2 <- aiMove game1
         setTimeout 10 $ loop game2
 
