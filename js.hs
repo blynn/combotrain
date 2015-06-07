ee801949e6f03db52ddef98046c0a5a9a2b523b2
Ben Lynn <benlynn@gmail.com>
2015-06-07 11:44:56 -0700

    Cedes control when there is nothing to do.
    Replaced magic constants with variables. Generalized parity check.

--- js.hs
+++ js.hs
@@ -1,74 +1,63 @@
+-- We can only receive mouse and keyboard events when none of our code is
+-- running or during the timeout, thus we could use IORef instead of MVar.
+--
+-- Our MVar contains Ready except when there is a pending timeout, which
+-- explains some of the logic.
 import Control.Concurrent.MVar
-import Control.Monad
 import Control.Applicative
+import Control.Monad
 import Data.Array
 import Haste
 import Haste.Graphics.Canvas
 
-bnds = ((0,0), (3,3)); frameCnt = 8; sz = 64
+data Event = Ready | Slide Int (Int, Int) (Int, Int) deriving Eq
 
-data Anim = Ready | Solved |
-   Slide { frame :: Int, r0 :: Int, c0 :: Int, r :: Int, c :: Int } deriving Eq
-data Event = KeyDown Int | Click Int Int
+m = 4; bnds = ((0, 0), (m - 1, m - 1)); frameCnt = 8; sz = 64
 
-handle board ev = let
-  (r0, c0) = head [i | i <- range bnds, board!i == 16]
-  (r , c ) = case ev of
-    KeyDown sym -> case sym of
-      38  -> (r0 + 1, c0)
-      40  -> (r0 - 1, c0)
-      37  -> (r0, c0 + 1)
-      39  -> (r0, c0 - 1)
-      _   -> (-1, -1)
-    Click x y -> (y `div` sz, x `div` sz)
-  in if inRange bnds (r, c) && (abs (c - c0) + abs (r - r0) == 1)
-    then Slide 0 r0 c0 r c else Ready
+parity []     = 0
+parity (x:xs) = (if x == m^2 then uncurry (+) $ divMod (length xs) m else 0)
+              + length (filter (x>) xs) + parity xs
 
-parity      [] = 0
-parity (16:xs) = (length xs `div` 4)     + parity xs
-parity ( x:xs) = length (filter (x>) xs) + parity xs
+rndPerm [] _     = []
+rndPerm xs seed0 = let (n, seed1) = randomR (0, length xs - 1) seed0
+                       (a, b:bs) = splitAt n xs
+                       in b:rndPerm (a ++ bs) seed1
 
-gen = do
-  seed <- newSeed
+gen = newSeed >>= \seed -> let z = rndPerm [1..m^2] seed
+  in if even $ parity z then return $ listArray bnds z else gen
+
+tile x y n = color (RGB c c c) $ fill $ rect (fromIntegral x, fromIntegral y)
+  (fromIntegral $ x + sz, fromIntegral $ y + sz)
+  where c = if n == m^2 then 0 else 255 - (225 * (n - 1) `div` (m^2 - 1))
+
+main = withElems ["body", "canvas"] $ \[body, cElem] -> do
+  Just canvas <- getCanvas cElem
+  ev <- newEmptyMVar
   let
-    f [x] _ = [x]
-    f xs seed = let
-      (n, seed1) = randomR (0, length xs - 1) seed
-      (a, b:bs) = splitAt n xs
-      in b:f (a ++ bs) seed1
-    z = f [1..16] seed
-  if even $ parity z then return $ listArray bnds z else gen
-
-grey n = color $ RGB c c c
-  where c = case n of 16 -> 0
-                      _  -> 255 - 15*(n - 1)
-
-main = withElems ["body", "canvas"] $ \[body, canvasElem] -> do
-  Just canvas <- getCanvas canvasElem
-  evq <- newMVar []
-  canvasElem  `onEvent` OnMouseDown $
-    \_ (x, y) -> takeMVar evq >>= putMVar evq . (++ [Click x y])
-  body `onEvent` OnKeyDown $
-    \k -> takeMVar evq >>= putMVar evq . (++ [KeyDown k])
-  let
-    tile x y n = grey n $ fill $ rect (fromIntegral x, fromIntegral y) (fromIntegral $ x + sz, fromIntegral $ y + sz)
-    animate board Ready = if [1..16] == elems board
-      then alert "A WINNER IS YOU!" >> return (board, Solved)
-      else return (board, Ready)
-    animate board Solved = return (board, Solved)
-    animate board slide@(Slide frame r0 c0 r c) = renderOnTop canvas $ do
-      tile (sz*c) (sz*r) 16
-      tile (sz*c + sz*(c0 - c) * frame `div` frameCnt)
-           (sz*r + sz*(r0 - r) * frame `div` frameCnt) (board!(r,c))
-      return $ if frame == frameCnt - 1
-        then (board // [((r,c), board!(r0,c0)), ((r0,c0), board!(r,c))], Ready)
-        else (board, slide{frame = frame + 1})
-    loop board anim = do
-      render canvas $ sequence_ $ (\((r, c), n) -> tile (sz*c) (sz*r) n) <$> assocs board
-      (board1, anim1) <- animate board anim
-      if anim1 == Solved then newGame else do
-        q <- swapMVar evq []
-        setTimeout 10 $ loop board1 $ if null q
-          then anim1 else handle board1 (head q)
-    newGame = gen >>= flip loop Ready
+    loop = takeMVar ev >>= \(q, b) -> do
+      render canvas $ forM_ (assocs b) $ \((r, c), n) -> tile (sz*c) (sz*r) n
+      if [1..m^2] == elems b then alert "You win!" >> newGame else case q of
+        Slide frame i0@(r0, c0) i@(r, c) -> do
+          renderOnTop canvas $ tile (sz*c) (sz*r) (m^2) >>
+            tile (sz*c + sz*(c0 - c) * frame `div` frameCnt)
+                 (sz*r + sz*(r0 - r) * frame `div` frameCnt) (b!(r,c))
+          if frame == frameCnt - 1
+            then putMVar ev (Ready, b // [(i, b!i0), (i0, b!i)]) >> loop
+            else putMVar ev (Slide (frame + 1) i0 i, b) >> setTimeout 10 loop
+        _ -> putMVar ev (q, b)
+    newGame = gen >>= putMVar ev . (,) Ready >> loop
+    try q i0@(r0, c0) i@(r, c) b =
+      if inRange bnds (r, c) && abs (c - c0) + abs (r - r0) == 1
+        then putMVar ev (Slide 0 i0 i, b) >> when (q == Ready) loop
+        else putMVar ev (q, b)
+  cElem `onEvent` OnMouseDown $ \_ (x, y) -> takeMVar ev >>= \(q, b) ->
+    try q (head [i | i <- range bnds, b!i == m^2]) (y `div` sz, x `div` sz) b
+  body  `onEvent` OnKeyDown   $ \k        -> takeMVar ev >>= \(q, b) -> let
+    (r0, c0) = head [i | i <- range bnds, b!i == m^2]
+    (r , c ) = case k of 38  -> (r0 + 1, c0)
+                         40  -> (r0 - 1, c0)
+                         37  -> (r0, c0 + 1)
+                         39  -> (r0, c0 - 1)
+                         _   -> (-1, -1)
+    in try q (r0, c0) (r, c) b
   newGame
