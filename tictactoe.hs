e9bb8cc7680f76b02a9d81c6859ffa66ce276038
Ben Lynn <benlynn@gmail.com>
2016-04-21 19:50:37 -0700

    tictactoe.hs: migrate to Haste 0.5.4.

--- tictactoe.hs
+++ tictactoe.hs
@@ -1,12 +1,13 @@
--- We use "setTimeout 1" to refresh the screen. Is there a cleaner way?
-import Control.Concurrent.MVar
 import Data.Array
-import Data.Function
+import Data.Bool
+import Data.IORef
 import Data.List
+import Data.Ord
 import Data.Tree
-import Control.Applicative
 import Control.Monad
 import Haste
+import Haste.DOM
+import Haste.Events
 import Haste.Graphics.Canvas
 
 sz = 64; bd = 4; bnds = ((0,0), (2,2))
@@ -21,14 +22,14 @@ goals = [join (,) <$> [0..2], (,) <*> (2-) <$> [0..2]]  -- Diagonals.
         ++ ((<$> [0..2]) .      (,) <$> [0..2])  -- Rows and columns.
         ++ ((<$> [0..2]) . flip (,) <$> [0..2])
 
-move (Game board0 Play player) i
-  | or $ and . ((== player) . (board!) <$>) <$> goals = Game board Won  player
-  | Nothing == find (== 0) (elems board)              = Game board Draw player
-  | otherwise                            = Game board Play $ -player
+move board0 player i
+  | or $ all ((== player) . (board!)) <$> goals = Game board Won  player
+  | 0 `notElem` elems board                     = Game board Draw player
+  | otherwise                                   = Game board Play $ -player
   where board = board0 // [(i, player)]
 
-nextMoves game@(Game board status _) = (game, case status of
-  Play -> [move game i | i <- range bnds, board!i == 0]
+nextMoves game@(Game board status p) = (game, case status of
+  Play -> [move board p i | i <- indices board, board!i == 0]
   _    -> [])
 
 gameTree = unfoldTree nextMoves
@@ -37,13 +38,12 @@ score (Game _ Won n) = n
 score _              = 0
 
 maximize (Node leaf [])   = leaf
-maximize (Node _    kids) = maximum (map minimize kids)
+maximize (Node _    kids) = maximum $ minimize <$> kids
 
 minimize (Node leaf [])   = leaf
-minimize (Node _    kids) = minimum (map maximize kids)
+minimize (Node _    kids) = minimum $ maximize <$> kids
 
-best xs = snd $ maximumBy (compare `on` fst) $
-  map (\x -> (minimize $ fmap score $ gameTree x, x)) xs
+best = maximumBy $ comparing $ minimize . fmap score . gameTree
 
 omitWith op ((g, ns):nss) = let
   omit _   [] = []
@@ -63,20 +63,20 @@ minimize' (Node _    kids) = omitWith (>=)
 
 bestAB ms = fst $ last . maximize' $ Node undefined $ map gameTree ms
 
-intRect x y w h = Rect (fromIntegral x) (fromIntegral y) (fromIntegral w) (fromIntegral h)
+f = fromIntegral
 
-oblong x y w h = fill $ rect (fromIntegral x, fromIntegral y) (fromIntegral (x + w), fromIntegral (y + h))
+oblong x y w h = fill $ rect (f x, f y) (f $ x + w, f $ y + h)
 
 main = withElems ["body", "canvas", "message", "noab"] $
                 \[body, cElem, message, noab] -> do
   xo <- loadBitmap "xo.png"
-  Just canvas <- getCanvas cElem
-  seedVar <- newSeed >>= newMVar
-  gameVar <- newMVar initGame
+  Just canvas <- fromElem cElem
+  seedVar <- newSeed >>= newIORef
+  gameVar <- newIORef initGame
   let
     randomRIO r = do
-      (a, seed1) <- randomR r <$> takeMVar seedVar
-      putMVar seedVar seed1
+      (a, seed1) <- randomR r <$> readIORef seedVar
+      writeIORef seedVar seed1
       return a
 
     shuffleIO [] = return []
@@ -90,31 +90,32 @@ main = withElems ["body", "canvas", "message", "noab"] $
       when (y /= 0) $ oblong (x * sz)           (y * sz) sz bd
       when (y /= 2) $ oblong (x * sz) (y * sz + sz - bd) sz bd
       -- Draw nought or cross when present.
-      when (p == -1) $ drawClipped xo (fromIntegral (x * sz), fromIntegral (y * sz)) (intRect 0 0 sz sz)
-      when (p ==  1) $ drawClipped xo (fromIntegral (x * sz), fromIntegral (y * sz)) (intRect sz 0 sz sz)
+      when (p /= 0) $ drawClipped xo (f $ x * sz, f $ y * sz) $
+        Rect (f $ bool 0 sz $ p > 0) 0 (f sz) (f sz)
 
     aiMove game = do
       shuffledMoves <- shuffleIO $ snd $ nextMoves game
-      disableAB <- getProp noab "checked"
+      disableAB <- ("true" ==) <$> getProp noab "checked"
       return $ if moveRandomly then head shuffledMoves else
-        if disableAB == "true" then best shuffledMoves else bestAB shuffledMoves
+        bool bestAB best disableAB shuffledMoves
 
-    go game = swapMVar gameVar game >> update
+    go game = writeIORef gameVar game >> update
 
     update = do
-      game@(Game board status player) <- readMVar gameVar
+      game@(Game board status player) <- readIORef gameVar
       render canvas $ mapM_ sq $ assocs board
       setProp message "innerHTML" $ case status of
         Won  -> ("X.O"!!(player + 1)) : " wins"
         Draw -> "Draw"
         Play -> if player == -1 then "X to move" else "Thinking..."
-      when (player == 1 && status == Play) $ setTimeout 1 $ aiMove game >>= go
+      when (player == 1 && status == Play) $ void $
+        setTimer (Once 1) $ aiMove game >>= go  -- Delay for redraw.
 
-  void $ cElem `onEvent` OnMouseDown $ \_ (x, y) -> do
-    game@(Game board status player) <- readMVar gameVar
+  _ <- cElem `onEvent` MouseDown $ \(MouseData (x, y) _ _) -> do
+    Game board status player <- readIORef gameVar
     let i = (x `div` sz, y `div` sz) in when (status == Play && player == -1
-       && inRange bnds i && board!i == 0) $ go $ move game i
+       && inRange bnds i && board!i == 0) $ go $ move board player i
 
-  void $ body `onEvent` OnKeyDown $ \k -> when (k == 113) $ go initGame
+  _ <- body `onEvent` KeyDown $ \k -> when (keyCode k == 113) $ go initGame
 
   update
